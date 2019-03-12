1eac1cbf3e7d805349745231591ee4869c91207a
ExoMaster <exomaster2986@gmail.com>
2019-03-11 22:13:22 -0400

    Add Echo command

---
+++ src/main/java/exomaster/graybot/modules/commands/Echo.java
@@ -0,0 +1,18 @@
+package exomaster.graybot.modules.commands;
+
+import exomaster.graybot.structures.Command;
+import org.javacord.api.entity.message.Message;
+
+public class Echo extends Command {
+    public Echo() {
+        this.callback = "echo";
+        this.name = "Echo";
+        this.description = "Repeats what you say.";
+        this.usage = "<message>";
+    }
+
+    @Override
+    public void run(Message message) {
+        message.getChannel().sendMessage(message.getContent().replaceFirst(".echo",""));
+    }
+}
